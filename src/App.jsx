89aa18d8edcdfe84be2e496c0448e454140cc4e5
Luis Rinascente <eulalio2411@gmail.com>
2024-08-05 18:51:28 -0400

    :v

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-
-import { useEffect, useRef, useState } from 'react';
-import './App.css';
-import useFetch from './hooks/useFetch';
-import LocationCard from './components/LocationCard';
-import ResidentCard from './components/ResidentCard';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import useFetch from "./hooks/useFetch";
+import LocationCard from "./components/LocationCard";
+import ResidentCard from "./components/ResidentCard";
 
 function App() {
-
   const randomId = Math.floor(Math.random() * 126) + 1;
   const [inputValue, setInputValue] = useState(randomId);
   const [location, getLocation, isLoading, hasError] = useFetch();
@@ -21,8 +19,8 @@ function App() {
   const handleSubmit = (event) => {
     event.preventDefault();
     setInputValue(textInput.current.value.trim().toLowerCase());
-    textInput.current.value = '';
-  }
+    textInput.current.value = "";
+  };
 
   // const pagination = () => {
   //   const quantity = 5;
@@ -32,41 +30,32 @@ function App() {
   console.log(location);
 
   return (
-    <div className='app'>
-      {
-        isLoading ?
+    <div className="app">
+      {isLoading ? (
         <h2>Loading...</h2>
-        :
+      ) : (
         <>
-          <h1 className='app__title'>Rick And Morty App</h1>
-          <form className='app__form' onSubmit={handleSubmit}>
-            <input className='app__form-input' ref={textInput} type="number" />
-            <button className='app__form-button'>Search</button>
+          <h1 className="app__title">Rick And Morty App</h1>
+          <form className="app__form" onSubmit={handleSubmit}>
+            <input className="app__form-input" ref={textInput} type="number" />
+            <button className="app__form-button">Search</button>
           </form>
-          {
-            hasError || !+inputValue ? 
-              <h2>👀 Hey! You most provide an id from 1 to 126</h2>
-              :
+          {hasError || !+inputValue ? (
+            <h2>👀 Hey! You most provide an id from 1 to 126</h2>
+          ) : (
             <>
-            <LocationCard
-              info={location}
-            />
-            <div className='app__container'>
-              {
-                location?.residents.map((character) => (
-                  <ResidentCard 
-                    key={character}
-                    info={character}
-                  />
-                ))
-              }
-        </div>
+              <LocationCard info={location} />
+              <div className="app__container">
+                {location?.residents.map((character) => (
+                  <ResidentCard key={character} info={character} />
+                ))}
+              </div>
             </>
-          }
+          )}
         </>
-      }
+      )}
     </div>
-  )
+  );
 }
 
 export default App;
